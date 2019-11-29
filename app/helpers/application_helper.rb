26fb39f160ecce190dc76cfd7170ab3d1a5bdc01
EduardoPicolo <eduardonpicolo@gmail.com>
2019-11-28 21:50:20 -0300

    Added style to Devise error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,29 +1,10 @@
 module ApplicationHelper
-    def my_devise_error_messages!
-    return "" if resource.errors.empty? && resource.rolable.errors.empty?
-
-    messages = rolable_messages = ""
-
-    if !resource.errors.empty?
-      messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+  def flash_class(level)
+    case level
+        when 'notice' then "alert alert-dismissable alert-info"
+        when 'success' then "alert alert-dismissable alert-success"
+        when 'error' then "alert alert-dismissable alert-danger"
+        when 'alert' then "alert alert-dismissable alert-danger"
     end
-
-    if !resource.rolable.errors.empty?
-      rolable_messages = resource.rolable.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    end
-
-    messages = messages + rolable_messages   
-    sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count + resource.rolable.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
-
-    html = <<-HTML
-    <div id="error_explanation">
-    <h2>#{sentence}</h2>
-    <ul>#{messages}</ul>
-    </div>
-    HTML
-
-    html.html_safe
   end
 end
