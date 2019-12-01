78daaad00e3210ad80c5d19102b8ed863e9853a3
EduardoPicolo <eduardonpicolo@gmail.com>
2019-12-01 20:52:03 -0300

    Fixed profile name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,11 @@ class PagesController < ApplicationController
 
     def profile
         @user = User.find(params[:id])
+        if @user.type == 'Customer'
+            @profile_name = @user.first_name + @user.last_name
+        elsif @user.type == 'Host'
+            @profile_name = @user.company_name
+        end
+
     end
 end
