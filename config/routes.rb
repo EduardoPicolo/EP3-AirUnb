f50f4decd7fe3321a0962dbe94d2a2b24b2e20c0
EduardoPicolo <eduardonpicolo@gmail.com>
2019-12-01 17:05:35 -0300

    Fixed after_signup_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  devise_for :users, :controllers => { :sessions => 'sessions' }, :skip => :registrations
-  devise_for :customers, :hosts, :skip => :sessions
+  devise_for :users, controllers: { sessions: 'sessions' }, :skip => :registrations
+  devise_for :customers, :hosts,  controllers: { registrations: 'users/registrations'}, :skip => :sessions
 
   resources :spots
 
