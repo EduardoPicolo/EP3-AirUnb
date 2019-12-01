1321dc251794c511bc02dd8ce146d53a381854d2
EduardoPicolo <eduardonpicolo@gmail.com>
2019-12-01 04:13:36 -0300

    Added links to user Profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
-    devise_group :user, contains: [:customer, :host]
-
     before_action :configure_permitted_parameters, if: :devise_controller?
-
+    
+    devise_group :user, contains: [:customer, :host]
+    
     protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :birthday])
-        # devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:sign_in, keys: [:first_name, :last_name, :phone, :birthday])
         devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :birthday])
     end
 end
